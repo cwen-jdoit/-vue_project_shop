91e6494f55a9a5565156360e43b16347571215b5
Jdoit_CW <2027356110@qq.com>
2020-07-07 19:32:07 +0800

    数据统计code

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ import Params from '../components/goods/Params.vue'
 import List from '../components/goods/List.vue'
 import Add from '../components/goods/Add.vue'
 import Order from '../components/order/Order.vue'
+import Report from '../components/report/report.vue'
 
 Vue.use(VueRouter)
 
@@ -25,33 +26,38 @@ const routes = [{
     component: Home,
     redirect: '/welcome',
     children: [{
-        path: '/welcome',
-        component: Welcome
-    }, {
-        path: '/users',
-        component: Users
-    }, {
-        path: '/rights',
-        component: Rights
-    }, {
-        path: '/roles',
-        component: Roles
-    }, {
-        path: '/categories',
-        component: Cate
-    }, {
-        path: '/params',
-        component: Params
-    }, {
-        path: '/goods',
-        component: List
-    }, {
-        path: '/goods/add',
-        component: Add
-    }, {
-        path: '/orders',
-        component: Order
-    }]
+            path: '/welcome',
+            component: Welcome
+        }, {
+            path: '/users',
+            component: Users
+        }, {
+            path: '/rights',
+            component: Rights
+        }, {
+            path: '/roles',
+            component: Roles
+        }, {
+            path: '/categories',
+            component: Cate
+        }, {
+            path: '/params',
+            component: Params
+        }, {
+            path: '/goods',
+            component: List
+        }, {
+            path: '/goods/add',
+            component: Add
+        }, {
+            path: '/orders',
+            component: Order
+        },
+        {
+            path: '/reports',
+            component: Report
+        }
+    ]
 }]
 
 const router = new VueRouter({
